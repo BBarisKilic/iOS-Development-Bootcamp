995efbfc8acaa3bc27aee51eef4c0c5676658f8b
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-07-31 21:18:43 +0200

    Initial commit.

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Bülent Barış Kılıç on 23.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
