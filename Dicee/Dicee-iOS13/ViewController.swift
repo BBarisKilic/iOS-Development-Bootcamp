3b7ccab5181d81e9a38de5764af702ea06c5a855
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-01 14:45:53 +0200

    User interface created.

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
