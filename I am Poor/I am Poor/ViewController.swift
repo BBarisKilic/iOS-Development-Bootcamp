357c47dd3465edaedbdcaf73c9eb5fd2229b71c0
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-01 11:59:15 +0200

    "I am Poor" created.

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Bülent Barış Kılıç on 1.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
