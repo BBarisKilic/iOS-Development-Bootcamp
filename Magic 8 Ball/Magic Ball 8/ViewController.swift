018c459c8534aa25c145d3100b03b9474221d34e
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-03 22:59:12 +0200

    Magic 8 Ball completed.

--- Magic 8 Ball/Magic Ball 8/ViewController.swift
+++ Magic 8 Ball/Magic Ball 8/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let answers = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.image = answers[0]
     }
 
 
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = answers[Int.random(in: 0...4)]
+    }
 }
 
