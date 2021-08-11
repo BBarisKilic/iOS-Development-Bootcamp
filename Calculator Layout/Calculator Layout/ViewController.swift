37dadc5ae719f784a26700961609d0572f9aa58f
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-11 16:19:33 +0200

    Calculator Layout created.

---
+++ Calculator Layout/Calculator Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator Layout
+//
+//  Created by Bülent Barış Kılıç on 11.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
