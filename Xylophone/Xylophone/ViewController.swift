4866738d79618c85fb5830edb36ad361ffed3976
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-12 23:29:21 +0200

    playSound func added.

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -1,21 +1,35 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func cKeyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf:  url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
