b9574bc0afb3de2016b10981e3558da4007e9291
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-13 20:42:20 +0200

    EggTimer UI completed.

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -3,6 +3,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
+    var remainingTime = 0
+    var timer = Timer()
+    
+    @IBAction func pressedLevel(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let cookingMode = sender.currentTitle!
+        
+        remainingTime = eggTimes[cookingMode]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if remainingTime > 0 {
+            print("\(remainingTime) seconds")
+            remainingTime -= 1
+        } else {
+            mainTitle.text = "DONE!"
+        }
+    }
+    
+    
 }
