108015b6f256dca6508f6acda614885f6f618d09
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-13 22:41:33 +0200

    EggTimer completed.

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -7,27 +8,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     
     let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
-    var remainingTime = 0
     var timer = Timer()
+    var totalTime = 0
+    var secondsPast = 0
+    var player: AVAudioPlayer?
     
     @IBAction func pressedLevel(_ sender: UIButton) {
         timer.invalidate()
+        secondsPast = 0
         
         let cookingMode = sender.currentTitle!
+        mainTitle.text = cookingMode
         
-        remainingTime = eggTimes[cookingMode]!
+        totalTime = eggTimes[cookingMode]!
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
-        if remainingTime > 0 {
-            print("\(remainingTime) seconds")
-            remainingTime -= 1
+        if secondsPast < totalTime {
+            secondsPast += 1
+            let progress = Float(secondsPast) / Float(totalTime)
+            progressBar.progress = progress
         } else {
             mainTitle.text = "DONE!"
+            playSound()
         }
     }
     
-    
+    func playSound() {
+            guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                player = try AVAudioPlayer(contentsOf:  url, fileTypeHint: AVFileType.mp3.rawValue)
+
+                guard let player = player else { return }
+
+                player.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
 }
