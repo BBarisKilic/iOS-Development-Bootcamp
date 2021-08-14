024834ca62d6e980ede9a48c8f198dbb57fe53b2
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-14 20:17:35 +0200

    Applied MVC design pattern.

---
+++ Quizzler/Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,9 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Bülent Barış Kılıç on 14.08.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
