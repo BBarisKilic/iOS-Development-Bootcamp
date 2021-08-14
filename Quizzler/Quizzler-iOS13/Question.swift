23c4544656eeb0f7cfe2f655d4fdd2bcdc1c10ec
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-14 12:36:55 +0200

    Question structure created.

---
+++ Quizzler/Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Bülent Barış Kılıç on 14.08.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var answer: String
+    
+    init(text: String, answer: String) {
+        self.text = text
+        self.answer = answer
+    }
+}
--- Quizzler/Quizzler-iOS13/ViewController.swift
+++ Quizzler/Quizzler-iOS13/ViewController.swift
@@ -8,9 +8,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        ["Four + Two is equal to Six.", "True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten", "False"],
+        Question(text: "Four + Two is equal to Six.", answer: "True"),
+        Question(text: "Five - Three is greater than One", answer: "True"),
+        Question(text: "Three + Eight is less than Ten", answer: "False"),
     ]
     
     var questionNumber = 0
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle!
-        let actualAnswer = quiz[questionNumber][1]
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
             print("Right!")
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
     
     func updateUI() {
         progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
     }
     
 }
