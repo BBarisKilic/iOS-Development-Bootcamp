3d2dd7f4bea4a8acb115e5e9178d50314e647ac1
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-16 22:01:11 +0200

    Added BMI calculation method.

--- BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -3,10 +3,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meterLabel: UILabel!
+    @IBOutlet weak var kilogramLabel: UILabel!
+    @IBOutlet weak var heightSliderOutlet: UISlider!
+    @IBOutlet weak var weightSliderOutlet: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        meterLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        kilogramLabel.text = String(format: "%.0f", sender.value) + "Kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let bmi = weightSliderOutlet.value / pow(heightSliderOutlet.value, 2)
+        print(bmi)
+    }
 }
 
