e303bdfbc0c486e6a211fad7bd9f55d0dce846ca
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-08-18 15:07:56 +0200

    Added models.

---
+++ BMI-Calculator/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Bülent Barış Kılıç on 18.08.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
--- BMI-Calculator/BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI-Calculator/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -2,13 +2,21 @@
 import Foundation
 
 class CalculatorBrain {
-    var bmi: Float = 0
+    var bmi: BMI?
     
     func getBMIValue() -> String {
-        return String(format: "%.1f", bmi)
+        return String(format: "%.1f", bmi?.value ?? 0.0)
     }
     
     func calculateBMI(height: Float, weight: Float) {
-        bmi =  weight / pow(height, 2)
+        let bmiValue =  weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: .blue)
+        } else if bmiValue > 18.5 && bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: .green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: .red)
+        }
     }
 }
