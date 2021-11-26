eda161ca01b0773b6fa15c318d85b71a20baf6d4
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-11-26 19:51:11 +0100

    Updated CalculatorViewController class.

--- Tipsy/Tipsy/CalculatorViewController.swift
+++ Tipsy/Tipsy/CalculatorViewController.swift
@@ -14,14 +14,37 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tipPercentage: Double = 0.0
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "20%":
+            tipPercentage = 0.2
+            twentyPctButton.isSelected = true
+            tenPctButton.isSelected = false
+            zeroPctButton.isSelected = false
+            break
+        case "10%":
+            tipPercentage = 0.1
+            twentyPctButton.isSelected = false
+            tenPctButton.isSelected = true
+            zeroPctButton.isSelected = false
+            break
+        default:
+            tipPercentage = 0.0
+            twentyPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            zeroPctButton.isSelected = true
+            break
+        }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tipPercentage)
     }
     
 }
